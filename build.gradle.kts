f43404ca25c9ebb3440c641a716c20f92ec4ee7e
intisy (Finn Birich) <finn@birich.de>
2026-06-14 01:12:18 +0200

    build(ci): consume core + cross-addon deps via github-gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     id("com.gradleup.shadow")
-    id("io.github.intisy.github-gradle")
+    id("io.github.intisy.github-gradle") version "1.8.3"
 }
 
 group = "io.github.sefiraat"
@@ -31,12 +31,12 @@ repositories {
 }
 
 dependencies {
-    compileOnly(files("../../core/Slimefun5/core/build/libs/Slimefun v5.0.0-UNOFFICIAL-MC26.1.2.jar"))
+    githubCompileOnly("Slimefun5:Slimefun5:v5.2.1")
     compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
     compileOnly("org.jetbrains:annotations:23.0.0")
 
-    compileOnly(files("../InfinityExpansion/build/libs/InfinityExpansion v1.0.0.jar"))
+    githubCompileOnly("Slimefun5:InfinityExpansion:v1.1.2")
     compileOnly("com.bgsoftware:WildChestsAPI:2024.1")
 }
 
