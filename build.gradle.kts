d49bc304c431768112114c90275f70628efe095f
intisy (Finn Birich) <finn@birich.de>
2026-07-08 00:07:55 +0200

    build: move gradle conventions to Slimefun5/gradle repo; slim addon build

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,9 @@ plugins {
 group = "io.github.sefiraat"
 description = "Networks is a Slimefun addon that brings item storage and transportation networks."
 
-// Shared Slimefun-addon build conventions (Java 8, spigot-api baseline, core dep, publish, shadow, version).
-apply(from = "https://raw.githubusercontent.com/Slimefun5/workflows/stable/slimefun-addon.gradle")
+apply(from = "https://raw.githubusercontent.com/Slimefun5/gradle/stable/slimefun-addon.gradle")
 
 repositories {
-    maven("https://jitpack.io")
     maven("https://nexus.neetgames.com/repository/maven-public/")
     maven("https://repo.bg-software.com/repository/api/")
 }
@@ -20,9 +18,4 @@ dependencies {
     compileOnly("org.jetbrains:annotations:23.0.0")
     githubCompileOnly("Slimefun5:InfinityExpansion:v1.1.2")
     compileOnly("com.bgsoftware:WildChestsAPI:2024.1")
-}
-
-tasks {
-    compileTestJava { enabled = false }
-    test { enabled = false }
 }
